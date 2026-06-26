4521be9979a7f0bef79527a2a3a6d61cc68341fa
willjing2008 <jingzongting@gmail.com>
2026-06-26 16:07:35 +0800

    fix: wire RevenueCat iOS key from env, stop leaking UUIDs/internals, fix app name

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
+import { LanguageProvider } from './app/hooks/useLanguage';
+import { SubscriptionProvider } from './app/hooks/useSubscription';
+import { ConfirmProvider } from './app/hooks/useConfirm';
+import { Toaster } from './app/components/ui/sonner';
+
+  createRoot(document.getElementById("root")!).render(<LanguageProvider>
+      <SubscriptionProvider>
+        <ConfirmProvider>
+          <App />
+        </ConfirmProvider>
+        <Toaster theme="dark" position="top-center" richColors />
+      </SubscriptionProvider>
+    </LanguageProvider>);
+  
